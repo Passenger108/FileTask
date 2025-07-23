7d24cf38dbe26ffb22381ce7f9d9863b1fd9dc8e
Passenger108 <157837251+Passenger108@users.noreply.github.com>
2025-07-23 19:55:58 +0530

    revoke and reopening task feature UI added

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,8 +2,13 @@
 import React from "react";
 import "./TaskCard.css";
 
+import LoginContext from "../ context/LoginContext";
+
 const TaskCard = ({ task }) => {
   const { taskTitle, taskDescription, status, taskDate, category } = task;
+  const secret = React.useContext(LoginContext)
+
+  const actionBtn = (status=="active"||status=="newTask")? 0:1
 
   return (
     <div className={`task-card ${status}`}>
@@ -14,10 +19,11 @@ const TaskCard = ({ task }) => {
         <div className="task-meta">
           <span className="task-date">{taskDate}</span>
           <span className="task-category">{category}</span>
+          {secret.loginCred.isAdmin&&<button className="task-btn">{actionBtn?"Re-open":"Revoke"}</button>}
         </div>
       </div>
     </div>
   );
 };
 
-export default TaskCard;
+export default TaskCard; 
